6e1c22a36d44f07e1fa98a7ed0c0895ab6c6faff
Adil Hussain <adilson05uk@gmail.com>
2021-05-07 13:01:55 +0100

    Changed the `ViewInteraction.waitForMatch(...)` extension function to now accept a variable number of matchers instead of just a single matcher.

--- library/src/main/java/com/tazkiyatech/utils/espresso/ViewInteractionExtensions.kt
+++ library/src/main/java/com/tazkiyatech/utils/espresso/ViewInteractionExtensions.kt
@@ -18,13 +18,13 @@ fun ViewInteraction.checkMatches(vararg matchers: Matcher<View>): ViewInteractio
 }
 
 /**
- * Waits up to [timeout] milliseconds for the view selected by the current view matcher to match the given [Matcher].
+ * Waits up to [timeout] milliseconds for the view selected by the current view matcher to match all of the given view matchers.
  *
  * This function makes Espresso tests easier to read and write by combining a series of calls into one.
  *
- * @param matcher the [Matcher] to wait for.
+ * @param matchers the array of [Matcher]s to wait for.
  * @param timeout the length of time in milliseconds to wait for.
  */
-fun ViewInteraction.waitForMatch(matcher: Matcher<View>, timeout: Long = 3000L): ViewInteraction {
-    return perform(ViewActions.waitForMatch(matcher, timeout))
+fun ViewInteraction.waitForMatch(vararg matchers: Matcher<View>, timeout: Long = 3000L): ViewInteraction {
+    return perform(ViewActions.waitForMatch(allOf(*matchers), timeout))
 }
