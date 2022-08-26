f60da225efbda63dc94fcd313803e62794a844a2
Adil Hussain <adilson05uk@gmail.com>
2022-08-26 13:06:29 +0100

    Added test for `TabViewMatchers.withTabText(text:)` matcher method.

--- library/src/androidTest/java/com/tazkiyatech/utils/espresso/TabViewMatchersTest.kt
+++ library/src/androidTest/java/com/tazkiyatech/utils/espresso/TabViewMatchersTest.kt
@@ -5,6 +5,7 @@ import android.view.ContextThemeWrapper
 import androidx.test.platform.app.InstrumentationRegistry
 import com.google.android.material.tabs.TabLayout
 import com.tazkiyatech.utils.espresso.TabViewMatchers.isSelectedTab
+import com.tazkiyatech.utils.espresso.TabViewMatchers.withTabText
 import com.tazkiyatech.utils.test.R
 import org.hamcrest.Matchers.not
 import org.junit.Assert.assertThat
@@ -37,5 +38,18 @@ class TabViewMatchersTest {
         // Then.
         assertThat(tab1.view, not(isSelectedTab()))
         assertThat(tab2.view, isSelectedTab())
+    }
+
+    @Test
+    fun withTabText_when_Tab_has_text() {
+        // Given.
+        val tabLayout = TabLayout(ContextThemeWrapper(context, R.style.Theme_AppCompat))
+        val tab = tabLayout.newTab()
+
+        // When.
+        tab.text = "Some Text"
+
+        // Then.
+        assertThat(tab.view, withTabText("Some Text"))
     }
 }
