9af1a2c51c56b0d95a080d544efd8165b1cc894c
Adil Hussain <>
2024-01-12 10:47:59 +0000

    Removed the "junit5_version" Gradle extra property and now using a Gradle version catalog for this instead.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-buildscript {
-    extra["junit5_version"] = "5.10.1"
-}
-
 plugins {
     id("com.android.library") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
